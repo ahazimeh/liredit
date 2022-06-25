ab29b8a4029dcf040e2285afc2a7161acc62c2ae
Ali Hazimeh <ahazimeh@globalistic.tech>
2022-06-25 03:13:12 +0300

    register/login

---
+++ liredit-server/src/entities/User.ts
@@ -0,0 +1,25 @@
+import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
+import { Field, ObjectType } from "type-graphql";
+
+@ObjectType() //to be able to use it as return type
+@Entity() //corresponds to db table
+export class User {
+  @Field() //to expose the field
+  @PrimaryKey()
+  id: number;
+
+  @Field(() => String)
+  @Property({ type: "date" })
+  createdAt? = new Date();
+
+  @Field(() => String)
+  @Property({ type: "date", onUpdate: () => new Date() })
+  updatedAt? = new Date();
+
+  @Field()
+  @Property({ type: "text", unique: true }) //removing this means it is just a field in the class and not a column
+  username!: string;
+
+  @Property({ type: "text" }) //removing this means it is just a field in the class and not a column
+  password!: string;
+}
--- liredit-server/src/mikro-orm.config.ts
+++ liredit-server/src/mikro-orm.config.ts
@@ -2,13 +2,14 @@ import { MikroORM } from "@mikro-orm/core";
 import path from "path";
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
+import { User } from "./entities/User";
 
 export default {
   migrations: {
     path: path.join(__dirname, "./migrations"),
   },
   allowGlobalContext: true,
-  entities: [Post], // no need for `entitiesTs` this way
+  entities: [Post, User], // no need for `entitiesTs` this way
   dbName: "lireddit1",
   type: "postgresql", // one of `mongo` | `mysql` | `mariadb` | `postgresql` | `sqlite`
   debug: !__prod__, //to debug what sequel is being executed
