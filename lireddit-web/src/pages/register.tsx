cc620ba9dbab83cf0d3c9300910b46e0e894d793
Ali Hazimeh <ahazimeh@globalistic.tech>
2022-06-26 10:18:11 +0300

    2:55

--- lireddit-web/src/pages/register.tsx
+++ lireddit-web/src/pages/register.tsx
@@ -1,41 +1,26 @@
 import React from "react";
 import { Formik, Form } from "formik";
-import {
-  FormControl,
-  FormLabel,
-  Input,
-  FormErrorMessage,
-  Box,
-  Button,
-} from "@chakra-ui/react";
-import Wrapper from "../components/wrapper";
+import { Box, Button } from "@chakra-ui/react";
+import Wrapper from "../components/Wrapper";
 import { InputField } from "../components/InputField";
-import { useMutation } from "urql";
+import { useRegisterMutation } from "../generated/graphql";
 
 interface registerProps {}
 
-const REGISTER_MUT = `
-mutation Register($username: String!, $password:String!) {
-    register(options:{username:$username, password:$password}) {
-      user {
-        id
-      }
-      errors {
-        field
-      }
-    }
-  }
-  `;
-
 const Register: React.FC<registerProps> = ({}) => {
-  const [, register] = useMutation(REGISTER_MUT);
+  const [, register] = useRegisterMutation();
   return (
     <Wrapper variant="small">
       <Formik
         initialValues={{ username: "", password: "" }}
-        onSubmit={async (values) => {
+        onSubmit={async (values, { setErrors }) => {
           //   console.log(values);
           const response = await register(values);
+          if (response.data?.register.errors) {
+            setErrors({
+              username: "Hey I am an error",
+            });
+          }
         }}
       >
         {({ isSubmitting }) => (
