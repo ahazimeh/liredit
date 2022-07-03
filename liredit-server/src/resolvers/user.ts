c5cbf52fea4fe455f963ccf7eee45758a72e117b
Ali Hazimeh <ahazimeh@globalistic.tech>
2022-07-03 19:00:32 +0300

    GraphQL Field Permissions

--- liredit-server/src/resolvers/user.ts
+++ liredit-server/src/resolvers/user.ts
@@ -4,10 +4,12 @@ import {
   Arg,
   Ctx,
   Field,
+  FieldResolver,
   Mutation,
   ObjectType,
   Query,
   Resolver,
+  Root,
 } from "type-graphql";
 import argon2 from "argon2";
 // import { EntityManager } from "@mikro-orm/postgresql";
@@ -33,8 +35,18 @@ class UserResponse {
   user?: User;
 }
 
-@Resolver()
+@Resolver(User)
 export class UserResolver {
+  @FieldResolver(() => String)
+  email1(@Root() user: User, @Ctx() { req }: MyContext) {
+    // this is the current user and it is ok to show them their own email
+    if (req.session.userId === user.id) {
+      return user.email;
+    }
+    // current user wants to see someone elses email
+    return "";
+  }
+
   @Mutation(() => UserResponse)
   async changePassword(
     @Arg("token") token: string,
